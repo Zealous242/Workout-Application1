ca657398b125478de9434bd16e8ab4ae451b1de1
yasum <yasum.abbas137@gmail.com>
2022-07-06 12:03:34 +0100

    first commit

---
+++ WorkoutApplication/Core/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WorkoutApplication.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;        //Using underscores here eliminates the need to use the this keyword
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged 
+        { 
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;   
+        }
+
+        public bool CanExecute(object parameter) 
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter) 
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ WorkoutApplication/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutApplication.Core;
+
+namespace WorkoutApplication.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand CalendarViewCommand { get; set; }  
+        public HomeViewModel HomeVm { get; set; }
+        public CalendarViewModel CalendarVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            {   _currentView = value; 
+                OnPropertyChanged(); //This ensures that the view changes
+            }
+        }
+
+        //The followinging code is used to ensure that the views load properly when the buttons in the navigation bar are clicked
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            CalendarVm = new CalendarViewModel();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = HomeVm;   
+            });
+
+            CalendarViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CalendarVm;
+            });
+
+        }
+    }
+}
