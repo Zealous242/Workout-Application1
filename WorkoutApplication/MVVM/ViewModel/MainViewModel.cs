cae9328c2d977eb095c1b46adac39b718983a4c6
yasum <yasum.abbas137@gmail.com>
2022-07-12 12:52:51 +0100

    Added new buttons to navigation bar

--- WorkoutApplication/MVVM/ViewModel/MainViewModel.cs
+++ WorkoutApplication/MVVM/ViewModel/MainViewModel.cs
@@ -9,6 +9,7 @@ namespace WorkoutApplication.MVVM.ViewModel
 {
     internal class MainViewModel : ObservableObject
     {
+        //The following relay commands allows switching between different views
         public RelayCommand HomeViewCommand { get; set; }
 
         public RelayCommand CalendarViewCommand { get; set; }
@@ -21,6 +22,20 @@ namespace WorkoutApplication.MVVM.ViewModel
 
         public RelayCommand NotesViewCommand { get; set; }
 
+        public RelayCommand MondayViewCommand { get; set; }
+
+        public RelayCommand TuesdayViewCommand { get; set; }
+
+        public RelayCommand WednesdayViewCommand { get; set; }
+
+        public RelayCommand ThursdayViewCommand { get; set; }
+
+        public RelayCommand FridayViewCommand { get; set; }
+
+        public RelayCommand SaturdayViewCommand { get; set; }
+
+        public RelayCommand SundayViewCommand { get; set; }
+
         public HomeViewModel HomeVm { get; set; }
 
         public CalendarViewModel CalendarVm { get; set; }
@@ -31,6 +46,20 @@ namespace WorkoutApplication.MVVM.ViewModel
         public StretchesViewModel StretchesVm { get; set; }
 
         public NotesViewModel NotesVm { get; set; }
+
+        public MondayViewModel MondayVm { get; set; }
+
+        public TuesdayViewModel TuesdayVm { get; set; }
+
+        public WednesdayViewModel WednesdayVm { get; set; }
+
+        public ThursdayViewModel ThursdayVm { get; set; }
+
+        public FridayViewModel FridayVm { get; set; }
+
+        public SaturdayViewModel SaturdayVm { get; set; }
+
+        public SundayViewModel SundayVm { get; set; }
 
         private object _currentView; //Having an underscore at the beginning of a variable name eliminates the need to use the this keyword
 
@@ -50,14 +79,21 @@ namespace WorkoutApplication.MVVM.ViewModel
             CalendarVm = new CalendarViewModel();
             WeeklyCalendarVm = new WeeklyCalendarViewModel();
             WorkoutsVm = new WorkoutsViewModel();
-            StretchesVm = new StretchesViewModel(); 
-            NotesVm = new NotesViewModel(); 
+            StretchesVm = new StretchesViewModel();
+            NotesVm = new NotesViewModel();
+            MondayVm = new MondayViewModel();
+            TuesdayVm = new TuesdayViewModel();
+            WednesdayVm = new WednesdayViewModel();
+            ThursdayVm = new ThursdayViewModel();
+            FridayVm = new FridayViewModel();
+            SaturdayVm = new SaturdayViewModel();
+            SundayVm = new SundayViewModel();
 
             CurrentView = HomeVm;
 
-            HomeViewCommand = new RelayCommand(o => 
-            { 
-                CurrentView = HomeVm;   
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
             });
 
             CalendarViewCommand = new RelayCommand(o =>
@@ -70,17 +106,45 @@ namespace WorkoutApplication.MVVM.ViewModel
                 CurrentView = WeeklyCalendarVm;
             });
 
-            WorkoutsViewCommand = new RelayCommand(o => 
-            { 
+            WorkoutsViewCommand = new RelayCommand(o =>
+            {
                 CurrentView = WorkoutsVm;
             });
 
-            StretchesViewCommand = new RelayCommand(o => { 
-                CurrentView = StretchesVm;  
+            StretchesViewCommand = new RelayCommand(o => {
+                CurrentView = StretchesVm;
             });
 
             NotesViewCommand = new RelayCommand(o => {
                 CurrentView = NotesVm;
+            });
+
+            MondayViewCommand = new RelayCommand(o => { 
+                CurrentView= MondayVm;  
+            });
+
+            TuesdayViewCommand = new RelayCommand(o => {
+                CurrentView = TuesdayVm;
+            });
+
+            WednesdayViewCommand = new RelayCommand(o => {
+                CurrentView = WednesdayVm;
+            });
+
+            ThursdayViewCommand = new RelayCommand(o => {
+                CurrentView = ThursdayVm;
+            });
+
+            FridayViewCommand = new RelayCommand(o => {
+                CurrentView = FridayVm;
+            });
+
+            SaturdayViewCommand = new RelayCommand(o => {
+                CurrentView = SaturdayVm;
+            });
+
+            SundayViewCommand = new RelayCommand(o => {
+                CurrentView = SundayVm;
             });
         }
     }
