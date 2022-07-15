5a35d1611be389dcb9c2f23d65aa58681e836758
yasum <yasum.abbas137@gmail.com>
2022-07-15 15:55:47 +0100

    Added Comments

--- WorkoutApplication/MVVM/ViewModel/FridayViewModel.cs
+++ WorkoutApplication/MVVM/ViewModel/FridayViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WorkoutApplication.MVVM.ViewModel
 {
-    internal class FridayViewModel
+    internal class FridayViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
